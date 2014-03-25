9ef9b39fdf8b2c6b67d1421a3a2fe42cbb402b79
agentu <agentu_pixel@wp.pl>
2014-03-25 13:17:18 +0100

    single-room server v 1.0 (obsługa sieci w kliecie też już zrobiona)

--- serwer/serwer/klient.cpp
+++ serwer/serwer/klient.cpp
@@ -1,79 +1,147 @@
-/* trzeba to skompilowac w osobnym projekcie i wtedy mo¿na testowaæ dzia³¹nie i dopisywaæ jakieœ œcierwa*/
+/* trzeba to skompilowac w osobnym projekcie i wtedy mo¿na testowaæ dzia³¹nie i dopisywaæ funkcje*/
 /*
+
 #include <iostream>
 #include <winsock2.h>
 #pragma comment(lib,"ws2_32.lib")
 
-int convert(char sign){
-int x = static_cast<int>(sign);
+int toint(char sign[]){
+int x = static_cast<int>(sign[0]);
 return x;
 }
 
-
-void clear_buffer(char buffer[], int i){
-for (int k = 0; k < i; k++)
-buffer[k] = 0;
+char tochar(int sign){
+char x;
+x = static_cast<char>(sign);
+return x;
 }
 
-int main(){
+int main()
+{
 
-//int x, y, m;
+int x = 1, y = 2, m;
+// Initialise Winsock
 WSADATA WsaDat;
-if (WSAStartup(MAKEWORD(2, 2), &WsaDat) != 0){
+if (WSAStartup(MAKEWORD(2, 2), &WsaDat) != 0)
+{
 std::cout << "Winsock error - Winsock initialization failed\r\n";
 WSACleanup();
 system("PAUSE");
 return 0;
 }
 
+// Create our socket
 SOCKET Socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-if (Socket == INVALID_SOCKET){
+if (Socket == INVALID_SOCKET)
+{
 std::cout << "Winsock error - Socket creation Failed!\r\n";
 WSACleanup();
 system("PAUSE");
 return 0;
 }
 
+// Resolve IP address for hostname
 struct hostent *host;
-if ((host = gethostbyname("localhost")) == NULL){
+if ((host = gethostbyname("localhost")) == NULL)
+{
 std::cout << "Failed to resolve hostname.\r\n";
 WSACleanup();
 system("PAUSE");
 return 0;
 }
 
+// Setup our socket address structure
 SOCKADDR_IN SockAddr;
 SockAddr.sin_port = htons(27015);
 SockAddr.sin_family = AF_INET;
 SockAddr.sin_addr.s_addr = *((unsigned long*)host->h_addr);
 
-if (connect(Socket, (SOCKADDR*)(&SockAddr), sizeof(SockAddr)) != 0){
+// Attempt to connect to server
+if (connect(Socket, (SOCKADDR*)(&SockAddr), sizeof(SockAddr)) != 0)
+{
 std::cout << "Failed to establish connection with server\r\n";
 WSACleanup();
 system("PAUSE");
 return 0;
 }
 
-
+// Display message from server
 char buffer[1], tempBuffer[1];
 memset(buffer, 0, 1);
 memset(tempBuffer, 0, 1);
 recv(Socket, buffer, 1, 0);
-tempBuffer[0] = buffer[0];
-if (tempBuffer[0] == 't')
-std::cout << "set target" << std::endl;
-else
-std::cout << "opponent is shooting" << std::endl;
+while (buffer[0] != 'l'){
+if (buffer[0] == 't'){
+std::cout << "wybierz cel" << std::endl;
+//todo: wywo³anie funkcji celowania, przypisanie  x i y
+buffer[0] = tochar(x);
+tempBuffer[0] = tochar(y);
+std::cout << "pierwszys" << std::endl;
+send(Socket, buffer, 1, 0);
+std::cout << "drugis" << std::endl;
+send(Socket, tempBuffer, 1, 0);
+std::cout << "pierwszyr" << std::endl;
 recv(Socket, buffer, 1, 0);
+std::cout << "drugir" << std::endl;
+recv(Socket, tempBuffer, 1, 0);
+std::cout << "koniec" << std::endl;
+if (buffer[0] == 'p')
+std::cout << "pud³o!" << std::endl;
+else
+if (buffer[0] == 't')
+std::cout << "trafiony!" << std::endl;
+else
+std::cout << "smieci";
 
-clear_buffer(buffer, 1);
+//todo: wywo³anie funkcji oznaczaj¹cej na planszy trafienie lub pud³o
+//na podstawie zawartoœci buffer[0]
 
-std::cout << std::endl;
-shutdown(Socket, SD_SEND);
 
-closesocket(Socket);
 
-WSACleanup();
-system("PAUSE");
-return 0;
-}*/
+
+		}
+		else
+			if (buffer[0] == 'p'){
+				std::cout << "celuje przeciwnik" << std::endl;
+				std::cout << "pierwszypr" << std::endl;
+				recv(Socket, buffer, 1, 0);
+				x = toint(buffer);
+				std::cout << "drugipr" << std::endl;
+				recv(Socket, tempBuffer, 1, 0);
+				std::cout << "koniecpr" << std::endl;
+				y = toint(buffer);
+				//todo: sprawdzenie, czy trafi³ i odpowiednie ustawienie buffer i tempbuffer
+				buffer[0] = 't';
+				tempBuffer[0] = 'n';
+				std::cout << "pierwszyps" << std::endl;
+				send(Socket, buffer, 1, 0);
+				std::cout << "drugips" << std::endl;
+				send(Socket, tempBuffer, 1, 0);
+				std::cout << "koniec" << std::endl;
+				buffer[0] = (buffer[0] == 't') ? 'p' : 't';
+				std::cout << buffer[0] << std::endl;
+			}
+	}
+
+
+	shutdown(Socket, SD_SEND);
+	closesocket(Socket);
+	WSACleanup();
+	system("PAUSE");
+	return 0;
+}
+
+
+
+
+*/
+
+/*
+send i recv operuj¹ jedynie na char [], wiêc nie przeœlemy struktury
+zawartoœci tempBuffer i buffer
+t - trafiony
+p - pudlo
+z - zatpiony
+n - niezatopiony
+l - przegrana - wysylana przez gracza, jesli juz nie ma zadnych statkow
+*/
--- serwer/serwer/main.cpp
+++ serwer/serwer/main.cpp
@@ -7,6 +7,8 @@ int main()
 {
 	srand(time(0));
 	int kto = rand() % 2;
+	std::cout << kto << std::endl;
+	system("pause");
 
 	WSADATA WsaDat;
 	if (WSAStartup(MAKEWORD(2, 2), &WsaDat) != 0)
@@ -59,24 +61,84 @@ int main()
 	Socket = TempSock;
 
 	std::cout << "Client connected!\r\n\r\n";
+	/*
+	send i recv operuj¹ jedynie na char [], wiêc nie przeœlemy struktury
+	t - trafiony
+	p - pudlo
+	z - zatopiony
+	n - niezatopiony
+	l - przegrana - wysylana przez gracza, jesli juz nie ma zadnych statkow
+	kto == 0 - strzela gracz 1 - losowane wy¿ej
+	kto == 1 - strzela gracz 2 -     - || -
+	
+	*/
 
-	char szMessage[] = "p";
-	char szMessage2[] = "n";
-	if (kto){
-		send(Socket, "t", strlen(szMessage), 0);
-		send(Socket, "n", strlen(szMessage2), 0);
-		send(Socket2, "p", strlen(szMessage), 0);
-		send(Socket2, "n", strlen(szMessage2), 0);
+
+
+	char szMessage[] = "t";
+	char szMessage2[] = "p";
+	if (kto == 0){
+		send(Socket, szMessage, 1, 0);
+		send(Socket2, szMessage2, 1, 0);
 	}
 	else{
-		send(Socket2, "t", strlen(szMessage), 0);
-		send(Socket2, "n", strlen(szMessage2), 0);
-		send(Socket, "p", strlen(szMessage), 0);
-		send(Socket, "n", strlen(szMessage2), 0);
+		send(Socket, szMessage2, 1, 0);
+		send(Socket2, szMessage, 1, 0);
+
 	}
-	/*while (true){
-	  pêtla g³ówna
-	}*/
+	while (kto<2){
+		if (kto == 0){
+			std::cout << "pierwszy recv 1" << std::endl;
+			recv(Socket, szMessage, 1, 0);
+			std::cout << "drugi recv 1" << std::endl;
+			recv(Socket, szMessage2, 1, 0);
+			std::cout << "pierwszy send 2" << std::endl;
+			send(Socket2, szMessage, 1, 0);
+			std::cout << "drugi send 2" << std::endl;
+			send(Socket2, szMessage2, 1, 0);
+			std::cout << "pierwszy recv 2" << std::endl;
+			recv(Socket2, szMessage, 1, 0);
+			std::cout << "drugi recv 2" << std::endl;
+			recv(Socket2, szMessage2, 1, 0);
+			std::cout << "pierwszy send 1" << std::endl;
+			send(Socket, szMessage, 1, 0);
+			std::cout << "drugi send 1" << std::endl;
+			send(Socket, szMessage2, 1, 0);
+			std::cout << "koniec" << std::endl;
+			std::cout << szMessage[0];
+			if (szMessage[0] == 't')
+				kto = 0;
+			else
+				kto = 1;
+		}
+		else 
+			if (kto == 1){
+				std::cout << "kupa" << std::endl;
+				std::cout << "pierwszy recv 1" << std::endl;
+				recv(Socket2, szMessage, 1, 0);
+				std::cout << "drugi recv 1" << std::endl;
+				recv(Socket2, szMessage2, 1, 0);
+				std::cout << "pierwszy send 2" << std::endl;
+				send(Socket, szMessage, 1, 0);
+				std::cout << "drugi send 2" << std::endl;
+				send(Socket, szMessage2, 1, 0);
+				std::cout << "pierwszy recv 2" << std::endl;
+				recv(Socket, szMessage, 1, 0);
+				std::cout << "drugi recv 2" << std::endl;
+				recv(Socket, szMessage2, 1, 0);
+				std::cout << "pierwszy send 1" << std::endl;
+				send(Socket2, szMessage, 1, 0);
+				std::cout << "drugi send 1" << std::endl;
+				send(Socket2, szMessage2, 1, 0);
+				std::cout << "koniec" << std::endl;
+				std::cout << szMessage[0];
+				if (szMessage[0] == 't')
+					kto = 1;
+				else
+					kto = 0;
+			}
+		
+	}
 	shutdown(Socket, SD_SEND);
 	closesocket(Socket);
 	shutdown(Socket2, SD_SEND);
